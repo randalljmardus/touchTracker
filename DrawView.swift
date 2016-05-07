474419dedba3ca2d603447f5ca6b37890f6282ba
Randall Mardus <randalljmardus@gmail.com>
2016-05-07 13:23:28 -0400

    initial commit

---
+++ DrawView.swift
@@ -0,0 +1,16 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Randall Mardus on 5/7/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    
+    var currentLine: Line?
+    var finishedLines = [Line]()
+    
+}
---
+++ Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Randall Mardus on 5/7/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
